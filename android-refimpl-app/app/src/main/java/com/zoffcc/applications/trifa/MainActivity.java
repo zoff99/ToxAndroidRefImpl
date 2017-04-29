cdbe55325c2bd5f5b9d3db6776888e73fc102c5f
Zoff <zoff99@users.noreply.github.com>
2017-04-29 13:36:00 +0200

    more setup

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
@@ -1,7 +1,8 @@
 package com.zoffcc.applications.trifa;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +12,20 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    static
+    {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
+    /*
+     * this is used to load the native library on
+	 * application startup. The library has already been unpacked at
+	 * installation time by the package manager.
+	 */
+    static
+    {
+        System.loadLibrary("jni-c-toxcore");
     }
 }
