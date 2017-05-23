8bf048c76ee165884630c76745666ecbf516665c
Zoff <zoff99@users.noreply.github.com>
2017-05-23 11:34:08 +0200

    disable for now

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/FriendlistArrayAdapter.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/FriendlistArrayAdapter.java
@@ -63,9 +63,9 @@ public class FriendlistArrayAdapter extends ArrayAdapter<FriendList>
     @Override
     public View getView(int position, View recycled, final ViewGroup parent)
     {
-        Log.i(TAG, "getView:fpubkey=" + values.get(position).tox_public_key_string);
-        Log.i(TAG, "getView:avatar_filename=" + values.get(position).avatar_filename);
-        Log.i(TAG, "getView:avatar_pathname=" + values.get(position).avatar_pathname);
+        // Log.i(TAG, "getView:fpubkey=" + values.get(position).tox_public_key_string);
+        // Log.i(TAG, "getView:avatar_filename=" + values.get(position).avatar_filename);
+        // Log.i(TAG, "getView:avatar_pathname=" + values.get(position).avatar_pathname);
 
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.friend_list_entry, parent, false);
@@ -77,100 +77,86 @@ public class FriendlistArrayAdapter extends ArrayAdapter<FriendList>
         statusText.setText(values.get(position).status_message);
 
         TextView unread_count = (TextView) rowView.findViewById(R.id.f_unread_count);
-        // Log.i(TAG, "unread_count view=" + unread_count);
 
         de.hdodenhof.circleimageview.CircleImageView avatar = (de.hdodenhof.circleimageview.CircleImageView) rowView.findViewById(R.id.f_avatar_icon);
         final Drawable d_lock = new IconicsDrawable(context).icon(FontAwesome.Icon.faw_lock).color(context.getResources().getColor(R.color.colorPrimaryDark)).sizeDp(24);
         avatar.setImageDrawable(d_lock);
 
-        final de.hdodenhof.circleimageview.CircleImageView avatar_f = avatar;
-        final int position_f = position;
-        try
+        if (1 == 2 - 2)
         {
-            // TODO: broken -------------------
-            // FriendList fl = orma.selectFromFriendList().tox_public_key_stringEq(values.get(position_f).tox_public_key_string).toList().get(0);
-
+            final de.hdodenhof.circleimageview.CircleImageView avatar_f = avatar;
+            final int position_f = position;
             try
             {
-                // Log.i(TAG, "getView:fl=" + fl);
-                // info.guardianproject.iocipher.FileReader fr = new info.guardianproject.iocipher.FileReader(fl.avatar_pathname + "/" + fl.avatar_filename);
-            }
-            catch (Exception e2)
-            {
-                e2.printStackTrace();
-                Log.i(TAG, "getView:EE2:" + e2.getMessage());
-            }
-
-            // load(new info.guardianproject.iocipher.File(fl.avatar_pathname + "/" + fl.avatar_filename)).
-
-            try
-            {
-                final Thread t = new Thread()
+                // TODO: broken -------------------
+                try
                 {
-                    @Override
-                    public void run()
+                    final Thread t = new Thread()
                     {
-                        try
+                        @Override
+                        public void run()
                         {
-                            info.guardianproject.iocipher.File f1 = new info.guardianproject.iocipher.File(values.get(position_f).avatar_pathname + "/" + values.get(position_f).avatar_filename);
-                            info.guardianproject.iocipher.FileInputStream fis = new info.guardianproject.iocipher.FileInputStream(f1);
-
-                            byte[] byteArray = new byte[(int) f1.length()];
-                            fis.read(byteArray, 0, (int) f1.length());
-                            final Bitmap bmp1 = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-
-                            Runnable myRunnable = new Runnable()
+                            try
                             {
-                                @Override
-                                public void run()
+                                info.guardianproject.iocipher.File f1 = new info.guardianproject.iocipher.File(values.get(position_f).avatar_pathname + "/" + values.get(position_f).avatar_filename);
+                                Log.i(TAG, "getView:f1=" + f1);
+                                info.guardianproject.iocipher.FileInputStream fis = new info.guardianproject.iocipher.FileInputStream(f1);
+
+                                byte[] byteArray = new byte[(int) f1.length()];
+                                fis.read(byteArray, 0, (int) f1.length());
+                                final Bitmap bmp1 = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+
+                                Runnable myRunnable = new Runnable()
                                 {
-                                    try
+                                    @Override
+                                    public void run()
                                     {
-                                        avatar_f.setImageBitmap(bmp1);
+                                        try
+                                        {
+                                            avatar_f.setImageBitmap(bmp1);
+                                        }
+                                        catch (Exception e)
+                                        {
+                                            Log.i(TAG, "getView:EE6:" + e.getMessage());
+                                            e.printStackTrace();
+                                        }
                                     }
-                                    catch (Exception e)
-                                    {
-                                        Log.i(TAG, "getView:EE6:" + e.getMessage());
-                                        e.printStackTrace();
-                                    }
-                                }
-                            };
-                            main_handler_s.post(myRunnable);
+                                };
+                                main_handler_s.post(myRunnable);
+                            }
+                            catch (Exception e)
+                            {
+                                Log.i(TAG, "getView:EE5:" + e.getMessage());
+                                e.printStackTrace();
+                            }
                         }
-                        catch (Exception e)
-                        {
-                            Log.i(TAG, "getView:EE5:" + e.getMessage());
-                            e.printStackTrace();
-                        }
-                    }
-                };
-                t.start();
+                    };
+                    t.start();
+                }
+                catch (Exception e)
+                {
+                    Log.i(TAG, "getView:EE4:" + e.getMessage());
+                    e.printStackTrace();
+                }
+
+
+                GlideApp.
+                        with(parent).
+                        load("").
+                        placeholder(d_lock).
+                        diskCacheStrategy(DiskCacheStrategy.AUTOMATIC).
+                        skipMemoryCache(false).
+                        into(avatar_f);
+                // TODO: broken -------------------
             }
             catch (Exception e)
             {
-                Log.i(TAG, "getView:EE4:" + e.getMessage());
                 e.printStackTrace();
+                Log.i(TAG, "getView:EE:" + e.getMessage());
             }
-
-
-            GlideApp.
-                    with(parent).
-                    load("").
-                    placeholder(d_lock).
-                    diskCacheStrategy(DiskCacheStrategy.NONE).
-                    skipMemoryCache(false).
-                    into(avatar_f);
-            // TODO: broken -------------------
-        }
-        catch (Exception e)
-
-        {
-            e.printStackTrace();
-            Log.i(TAG, "getView:EE:" + e.getMessage());
         }
 
         try
-
         {
             int new_messages_count = orma.selectFromMessage().tox_friendpubkeyEq(values.get(position).tox_public_key_string).and().is_newEq(true).count();
             if (new_messages_count > 0)
