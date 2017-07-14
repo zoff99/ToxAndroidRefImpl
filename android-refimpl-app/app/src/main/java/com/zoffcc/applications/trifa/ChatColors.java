480dd415a76e179cd2f442e098087ff53ed414a4
Zoff <zoff99@users.noreply.github.com>
2017-07-14 14:14:37 +0200

    add

---
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/ChatColors.java
@@ -0,0 +1,22 @@
+package com.zoffcc.applications.trifa;
+
+public class ChatColors
+{
+    static int[] PeerAvatarColors = {
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10
+    };
+
+    static int get_size()
+    {
+        return PeerAvatarColors.length;
+    }
+}
