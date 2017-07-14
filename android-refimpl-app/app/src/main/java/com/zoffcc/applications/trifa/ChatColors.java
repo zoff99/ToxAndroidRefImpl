1a0ec47abc750b604c8d8806ee7bd2155136e22b
Zoff <zoff99@users.noreply.github.com>
2017-07-14 14:31:51 +0200

    add actual colors

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/ChatColors.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/ChatColors.java
@@ -1,18 +1,22 @@
 package com.zoffcc.applications.trifa;
 
+import android.graphics.Color;
+
 public class ChatColors
 {
     static int[] PeerAvatarColors = {
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10
+            Color.parseColor("#0000FF"),
+            Color.parseColor("#FF00FF"),
+            Color.parseColor("#00FFFF"),
+            Color.parseColor("#008000"),
+            Color.parseColor("#00FF00"),
+            Color.parseColor("#800000"),
+            Color.parseColor("#000080"),
+            Color.parseColor("#808000"),
+            Color.parseColor("#800080"),
+            Color.parseColor("#FF0000"),
+            Color.parseColor("#008080"),
+            Color.parseColor("#FFFF00")
     };
 
     static int get_size()
