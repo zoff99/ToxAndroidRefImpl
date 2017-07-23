b13db011da906d20306060fb2f5da65b1e5d11be
Zoff <zoff99@users.noreply.github.com>
2017-07-23 14:21:28 +0200

    comment

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/TRIFAGlobals.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/TRIFAGlobals.java
@@ -46,6 +46,7 @@ public class TRIFAGlobals
     final static boolean DELETE_SQL_AND_VFS_ON_ERROR = false; // true -> will delete all data on any ERROR with SQL and VFS !!!
 
     final static String VFS_TMP_FILE_DIR = "/tempdir/files/";
+    // final static String VFS_TMP_AVATAR_DIR = "/avatar_tempdir/files/"; // TODO: avatar should get their own directory!
     final static String VFS_FILE_DIR = "/datadir/files/";
     final static String VFS_OWN_AVATAR_DIR = "/datadir/myavatar/";
     static String VFS_PREFIX = ""; // only set for normal (unencrypted) storage
