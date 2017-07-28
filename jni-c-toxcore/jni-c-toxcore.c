a73b5a6ac0fda61c892e7ed2e44160c16f6bd3d0
Zoff <zoff99@users.noreply.github.com>
2017-07-28 15:37:11 +0200

    0.99.15

--- jni-c-toxcore/jni-c-toxcore.c
+++ jni-c-toxcore/jni-c-toxcore.c
@@ -58,8 +58,8 @@
 // ----------- version -----------
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 99
-#define VERSION_PATCH 13
-static const char global_version_string[] = "0.99.13";
+#define VERSION_PATCH 15
+static const char global_version_string[] = "0.99.15";
 // ----------- version -----------
 // ----------- version -----------
 
