e2d10e45d0f47a14464c0963aa12a17d1c620b5a
Zoff <zoff99@users.noreply.github.com>
2017-08-15 18:17:42 +0200

    Update MainActivity.java

--- java_ref_client/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
+++ java_ref_client/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
@@ -19,6 +19,23 @@
 
 package com.zoffcc.applications.trifa;
 
+//  ==================================================
+//  compile with:
+//  javac MainActivity.java TrifaToxService.java  TRIFAGlobals.java ToxVars.java
+//  ==================================================
+
+/*
+   javac com/zoffcc/applications/trifa/MainActivity.java
+   javac com/zoffcc/applications/trifa/ToxVars.java
+   javac com/zoffcc/applications/trifa/TRIFAGlobals.java
+   javac com/zoffcc/applications/trifa/TrifaToxService.java
+*/
+
+
+
+
+import static com.zoffcc.applications.trifa.TRIFAGlobals.bootstrapping;
+
 public class MainActivity
 {
     private static final String TAG = "trifa.MainActivity";
@@ -33,23 +50,366 @@ public class MainActivity
     // --------- global config ---------
     // --------- global config ---------
 
-	public static void main(String[] args)
-	{
-		// Prints "Hello, World" in the terminal window.
-      		System.out.println("Hello, World");
-	}
+    static TrifaToxService tox_service_fg = null;
+    static boolean native_lib_loaded = false;
+    static long[] friends = null;
 
-	static
-	{
-		try
-		{
-    			System.loadLibrary("jni-c-toxcore");
-		}
-		catch (UnsatisfiedLinkError e)
-		{
-      			System.err.println("Native code library failed to load.\n" + e);
-			System.exit(1);
-		}
-	}
+    static class Log
+    {
+        public static void i(String tag, String message)
+        {
+            System.out.println("" + tag + ":" + message + "\n");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+
+        TrifaToxService.TOX_SERVICE_STARTED = false;
+        bootstrapping = false;
+        Log.i(TAG, "java.library.path:" + System.getProperty("java.library.path"));
+
+        Log.i(TAG, "loaded:c-toxcore:v" + tox_version_major() + "." + tox_version_minor() + "." + tox_version_patch());
+        Log.i(TAG, "loaded:jni-c-toxcore:v" + jnictoxcore_version());
+
+
+        tox_service_fg = new TrifaToxService();
+
+        if (!TrifaToxService.TOX_SERVICE_STARTED)
+        {
+            tox_service_fg.tox_thread_start_fg();
+        }
+
+
+    }
+
+
+    static
+    {
+        try
+        {
+            System.loadLibrary("jni-c-toxcore");
+            native_lib_loaded = true;
+            Log.i(TAG, "successfully loaded native library");
+        }
+        catch (java.lang.UnsatisfiedLinkError e)
+        {
+            native_lib_loaded = false;
+            Log.i(TAG, "loadLibrary jni-c-toxcore failed!");
+            e.printStackTrace();
+        }
+    }
+
+
+    // -------- native methods --------
+    // -------- native methods --------
+    // -------- native methods --------
+    public native void init(String data_dir, int udp_enabled, int orbot_enabled, String orbot_host, long orbot_port);
+
+    public native String getNativeLibAPI();
+
+    public static native void update_savedata_file();
+
+    public static native String get_my_toxid();
+
+    public static native void bootstrap();
+
+    public static native int add_tcp_relay_single(String ip, String key_hex, long port);
+
+    public static native int bootstrap_single(String ip, String key_hex, long port);
+
+    public static native void init_tox_callbacks();
+
+    public static native long tox_iteration_interval();
+
+    public static native long tox_iterate();
+
+    public static native long tox_kill();
+
+    public static native void exit();
+
+    public static native long tox_friend_send_message(long friendnum, int a_TOX_MESSAGE_TYPE, String message);
+
+    public static native long tox_version_major();
+
+    public static native long tox_version_minor();
+
+    public static native long tox_version_patch();
+
+    public static native String jnictoxcore_version();
+
+    public static native long tox_max_filename_length();
+
+    public static native long tox_file_id_length();
+
+    public static native long tox_max_message_length();
+
+    public static native long tox_friend_add(String toxid_str, String message);
+
+    public static native long tox_friend_add_norequest(String public_key_str);
+
+    public static native long tox_self_get_friend_list_size();
+
+    public static native void tox_self_set_nospam(long nospam); // this actually needs an "uint32_t" which is an unsigned 32bit integer value
+
+    public static native long tox_self_get_nospam(); // this actually returns an "uint32_t" which is an unsigned 32bit integer value
+
+    public static native long tox_friend_by_public_key(String friend_public_key_string);
+
+    public static native String tox_friend_get_public_key(long friend_number);
+
+    public static native long[] tox_self_get_friend_list();
+
+    public static native int tox_self_set_name(String name);
+
+    public static native int tox_self_set_status_message(String status_message);
+
+    public static native void tox_self_set_status(int a_TOX_USER_STATUS);
+
+    public static native int tox_self_set_typing(long friend_number, int typing);
+
+    public static native int tox_friend_get_connection_status(long friend_number);
+
+    public static native int tox_friend_delete(long friend_number);
+
+    public static native String tox_self_get_name();
+
+    public static native long tox_self_get_name_size();
+
+    public static native long tox_self_get_status_message_size();
+
+    public static native String tox_self_get_status_message();
+
+    public static native int tox_file_control(long friend_number, long file_number, int a_TOX_FILE_CONTROL);
+
+    public static native int tox_hash(java.nio.ByteBuffer hash_buffer, java.nio.ByteBuffer data_buffer, long data_length);
+
+    public static native int tox_file_seek(long friend_number, long file_number, long position);
+
+    public static native int tox_file_get_file_id(long friend_number, long file_number, java.nio.ByteBuffer file_id_buffer);
+
+    public static native long tox_file_send(long friend_number, long kind, long file_size, java.nio.ByteBuffer file_id_buffer, String file_name, long filename_length);
+
+    public static native int tox_file_send_chunk(long friend_number, long file_number, long position, java.nio.ByteBuffer data_buffer, long data_length);
+
+    // --------------- Conference -------------
+    // --------------- Conference -------------
+    // --------------- Conference -------------
+
+    public static native long tox_conference_join(long friend_number, java.nio.ByteBuffer cookie_buffer, long cookie_length);
+
+    public static native String tox_conference_peer_get_public_key(long conference_number, long peer_number);
+
+    public static native long tox_conference_peer_count(long conference_number);
+
+    public static native long tox_conference_peer_get_name_size(long conference_number, long peer_number);
+
+    public static native String tox_conference_peer_get_name(long conference_number, long peer_number);
+
+    public static native int tox_conference_peer_number_is_ours(long conference_number, long peer_number);
+
+    public static native long tox_conference_get_title_size(long conference_number);
+
+    public static native String tox_conference_get_title(long conference_number);
+
+    public static native int tox_conference_get_type(long conference_number);
+
+    public static native int tox_conference_send_message(long conference_number, int a_TOX_MESSAGE_TYPE, String message);
+
+    public static native int tox_conference_delete(long conference_number);
+    // --------------- Conference -------------
+    // --------------- Conference -------------
+    // --------------- Conference -------------
+
+
+    // --------------- AV -------------
+    // --------------- AV -------------
+    // --------------- AV -------------
+    public static native int toxav_answer(long friendnum, long audio_bit_rate, long video_bit_rate);
+
+    public static native long toxav_iteration_interval();
+
+    public static native int toxav_call(long friendnum, long audio_bit_rate, long video_bit_rate);
+
+    public static native int toxav_bit_rate_set(long friendnum, long audio_bit_rate, long video_bit_rate);
+
+    public static native int toxav_call_control(long friendnum, int a_TOXAV_CALL_CONTROL);
+
+    public static native int toxav_video_send_frame_uv_reversed(long friendnum, int frame_width_px, int frame_height_px);
+
+    public static native int toxav_video_send_frame(long friendnum, int frame_width_px, int frame_height_px);
+
+    public static native long set_JNI_video_buffer(java.nio.ByteBuffer buffer, int frame_width_px, int frame_height_px);
+
+    public static native void set_JNI_video_buffer2(java.nio.ByteBuffer buffer2, int frame_width_px, int frame_height_px);
+
+    public static native void set_JNI_audio_buffer(java.nio.ByteBuffer audio_buffer);
+
+    // buffer2 is for incoming audio
+    public static native void set_JNI_audio_buffer2(java.nio.ByteBuffer audio_buffer2);
+
+    /**
+     * Send an audio frame to a friend.
+     * <p>
+     * The expected format of the PCM data is: [s1c1][s1c2][...][s2c1][s2c2][...]...
+     * Meaning: sample 1 for channel 1, sample 1 for channel 2, ...
+     * For mono audio, this has no meaning, every sample is subsequent. For stereo,
+     * this means the expected format is LRLRLR... with samples for left and right
+     * alternating.
+     *
+     * @param friend_number The friend number of the friend to which to send an
+     *                      audio frame.
+     * @param sample_count  Number of samples in this frame. Valid numbers here are
+     *                      ((sample rate) * (audio length) / 1000), where audio length can be
+     *                      2.5, 5, 10, 20, 40 or 60 millseconds.
+     * @param channels      Number of audio channels. Supported values are 1 and 2.
+     * @param sampling_rate Audio sampling rate used in this frame. Valid sampling
+     *                      rates are 8000, 12000, 16000, 24000, or 48000.
+     */
+    public static native int toxav_audio_send_frame(long friend_number, long sample_count, int channels, long sampling_rate);
+    // --------------- AV -------------
+    // --------------- AV -------------
+    // --------------- AV -------------
+
+    // -------- native methods --------
+    // -------- native methods --------
+    // -------- native methods --------
+
+    // -------- called by AV native methods --------
+    // -------- called by AV native methods --------
+    // -------- called by AV native methods --------
+
+    static void android_toxav_callback_call_cb_method(long friend_number, int audio_enabled, int video_enabled)
+    {
+    }
+
+    static void android_toxav_callback_video_receive_frame_cb_method(long friend_number, long frame_width_px, long frame_height_px, long ystride, long ustride, long vstride)
+    {
+    }
+
+    static void android_toxav_callback_call_state_cb_method(long friend_number, int a_TOXAV_FRIEND_CALL_STATE)
+    {
+    }
+
+    static void android_toxav_callback_bit_rate_status_cb_method(long friend_number, long audio_bit_rate, long video_bit_rate)
+    {
+    }
+
+    static void android_toxav_callback_audio_receive_frame_cb_method(long friend_number, long sample_count, int channels, long sampling_rate)
+    {
+    }
+
+
+    // -------- called by AV native methods --------
+    // -------- called by AV native methods --------
+    // -------- called by AV native methods --------
+
+
+    // -------- called by native methods --------
+    // -------- called by native methods --------
+    // -------- called by native methods --------
+
+    static void android_tox_callback_self_connection_status_cb_method(int a_TOX_CONNECTION)
+    {
+    }
+
+    static void android_tox_callback_friend_name_cb_method(long friend_number, String friend_name, long length)
+    {
+    }
+
+    static void android_tox_callback_friend_status_message_cb_method(long friend_number, String status_message, long length)
+    {
+    }
+
+    static void android_tox_callback_friend_status_cb_method(long friend_number, int a_TOX_USER_STATUS)
+    {
+    }
+
+    static void android_tox_callback_friend_connection_status_cb_method(long friend_number, int a_TOX_CONNECTION)
+    {
+    }
+
+    static void android_tox_callback_friend_typing_cb_method(long friend_number, final int typing)
+    {
+    }
+
+    static void android_tox_callback_friend_read_receipt_cb_method(long friend_number, long message_id)
+    {
+    }
+
+    static void android_tox_callback_friend_request_cb_method(String friend_public_key, String friend_request_message, long length)
+    {
+    }
+
+    static void android_tox_callback_friend_message_cb_method(long friend_number, int message_type, String friend_message, long length)
+    {
+    }
+
+    static void android_tox_callback_file_recv_control_cb_method(long friend_number, long file_number, int a_TOX_FILE_CONTROL)
+    {
+    }
+
+    static void android_tox_callback_file_chunk_request_cb_method(long friend_number, long file_number, long position, long length)
+    {
+    }
+
+    static void android_tox_callback_file_recv_cb_method(long friend_number, long file_number, int a_TOX_FILE_KIND, long file_size, String filename, long filename_length)
+    {
+    }
+
+    static void android_tox_callback_file_recv_chunk_cb_method(long friend_number, long file_number, long position, byte[] data, long length)
+    {
+    }
+
+    static void android_tox_log_cb_method(int a_TOX_LOG_LEVEL, String file, long line, String function, String message)
+    {
+        if (CTOXCORE_NATIVE_LOGGING)
+        {
+            Log.i(TAG, "C-TOXCORE:" + ToxVars.TOX_LOG_LEVEL.value_str(a_TOX_LOG_LEVEL) + ":file=" + file + ":linenum=" + line + ":func=" + function + ":msg=" + message);
+        }
+    }
+
+    // -------- called by native methods --------
+    // -------- called by native methods --------
+    // -------- called by native methods --------
+
+    // -------- called by native Conference methods --------
+    // -------- called by native Conference methods --------
+    // -------- called by native Conference methods --------
+
+    static void android_tox_callback_conference_invite_cb_method(long friend_number, int a_TOX_CONFERENCE_TYPE, byte[] cookie_buffer, long cookie_length)
+    {
+    }
+
+
+    static void android_tox_callback_conference_message_cb_method(long conference_number, long peer_number, int a_TOX_MESSAGE_TYPE, String message, long length)
+    {
+    }
+
+    static void android_tox_callback_conference_title_cb_method(long conference_number, long peer_number, String title, long title_length)
+    {
+    }
+
+
+    static void android_tox_callback_conference_namelist_change_cb_method(long conference_number, long peer_number, int a_TOX_CONFERENCE_STATE_CHANGE)
+    {
+    }
+
+    // -------- called by native Conference methods --------
+    // -------- called by native Conference methods --------
+    // -------- called by native Conference methods --------
+
+
+    static int add_tcp_relay_single_wrapper(String ip, long port, String key_hex)
+    {
+        return add_tcp_relay_single(ip, key_hex, port);
+    }
+
+    static int bootstrap_single_wrapper(String ip, long port, String key_hex)
+    {
+        return bootstrap_single(ip, key_hex, port);
+    }
 }
 
