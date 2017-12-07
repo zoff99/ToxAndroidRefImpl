f8f3a2ed77270c6eb2b8d5e61243784961ad8041
Zoff <zoff99@users.noreply.github.com>
2017-12-07 18:48:04 +0100

    set higher default audio bitrate

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
@@ -287,7 +287,7 @@ public class MainActivity extends AppCompatActivity
     private static final String ALLOWED_CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!§$%&()=?,.;:-_+";
     static boolean PREF__software_echo_cancel = false;
     static int PREF__higher_video_quality = 1;
-    static int PREF__higher_audio_quality = 0;
+    static int PREF__higher_audio_quality = 1;
     static int PREF__udp_enabled = 0; // 0 -> Tox TCP mode, 1 -> Tox UDP mode
     static int PREF__audiosource = 2; // 1 -> VOICE_COMMUNICATION, 2 -> VOICE_RECOGNITION
     static boolean PREF__orbot_enabled = false;
@@ -505,12 +505,12 @@ public class MainActivity extends AppCompatActivity
 
         try
         {
-            PREF__higher_audio_quality = Integer.parseInt(settings.getString("higher_audio_quality", "0"));
+            PREF__higher_audio_quality = Integer.parseInt(settings.getString("higher_audio_quality", "1"));
         }
         catch (Exception e)
         {
             e.printStackTrace();
-            PREF__higher_audio_quality = 0;
+            PREF__higher_audio_quality = 1;
         }
 
         if (PREF__higher_audio_quality == 2)
@@ -1708,12 +1708,12 @@ public class MainActivity extends AppCompatActivity
 
         try
         {
-            PREF__higher_audio_quality = Integer.parseInt(settings.getString("higher_audio_quality", "0"));
+            PREF__higher_audio_quality = Integer.parseInt(settings.getString("higher_audio_quality", "1"));
         }
         catch (Exception e)
         {
             e.printStackTrace();
-            PREF__higher_audio_quality = 0;
+            PREF__higher_audio_quality = 1;
         }
 
         if (PREF__higher_audio_quality == 2)
