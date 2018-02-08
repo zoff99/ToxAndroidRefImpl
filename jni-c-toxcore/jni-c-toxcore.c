ec4265c204fe49bd8072f241e8b57859c4871e4a
Zoff <zoff99@users.noreply.github.com>
2018-02-08 14:46:27 +0000

    add first function

--- jni-c-toxcore/jni-c-toxcore.c
+++ jni-c-toxcore/jni-c-toxcore.c
@@ -2650,6 +2650,34 @@ TOX_USER_STATUS tox_friend_get_status(const Tox *tox, uint32_t friend_number, TO
  */
 #ifdef TOX_MESSAGE_V2_ACTIVE
 
+JNIEXPORT jlong JNICALL
+Java_com_zoffcc_applications_trifa_MainActivity_tox_1messagev2_1size(JNIEnv *env, jobject thiz, jlong text_length,
+jlong type, jlong alter_type)
+{
+	uint32_t tox_msg_size = tox_messagev2_size((uint32_t)text_length, (uint32_t)type, (uint32_t)alter_type);
+
+    return (jlong)tox_msg_size;
+}
+
+
+// uint32_t tox_messagev2_size(uint32_t text_length, uint32_t type, uint32_t alter_type);
+// bool tox_messagev2_wrap(uint32_t text_length, uint32_t type,
+//                        uint32_t alter_type,
+//                        uint8_t *message_text, uint32_t ts_sec,
+//                        uint16_t ts_ms, uint8_t *raw_message,
+//                        uint8_t *msgid);
+
+
+// bool tox_messagev2_get_message_id(uint8_t *raw_message, uint8_t *msg_id);
+// bool tox_messagev2_get_message_alter_id(uint8_t *raw_message, uint8_t *alter_id);
+// uint8_t tox_messagev2_get_alter_type(uint8_t *raw_message);
+// uint32_t tox_messagev2_get_ts_sec(uint8_t *raw_message);
+// uint16_t tox_messagev2_get_ts_ms(uint8_t *raw_message);
+// bool tox_messagev2_get_message_text(uint8_t *raw_message, uint32_t raw_message_len,
+//                                     bool is_alter_msg,
+//                                     uint32_t alter_type, uint8_t *message_text);
+
+
 #endif
 /*
  * ------------------------------------------------------------
