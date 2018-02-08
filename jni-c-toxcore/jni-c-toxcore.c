f788082f2013e608d71e78b6cecdb2017cf876ca
Zoff <zoff99@users.noreply.github.com>
2018-02-08 12:09:37 +0100

    add compat layer and MSGV2 define

--- jni-c-toxcore/jni-c-toxcore.c
+++ jni-c-toxcore/jni-c-toxcore.c
@@ -66,6 +66,79 @@ static const char global_version_string[] = "0.99.18";
 
 
 
+/*
+ * ------------------------------------------------------------
+ * ----------------- MessageV2 --------------------------------
+ * ------------------------------------------------------------
+ */
+#define _MESSAGEV2_ 1
+
+#ifdef _MESSAGEV2_
+
+#endif
+/*
+ * ------------------------------------------------------------
+ * ----------------- MessageV2 --------------------------------
+ * ------------------------------------------------------------
+ */
+
+
+
+
+/*
+ * ------------------------------------------------------------
+ * TOXCORE compatibility layer --------------------------------
+ * ------------------------------------------------------------
+ */
+#ifndef TOXCOMPAT_H_
+#define TOXCOMPAT_H_
+
+#if TOX_VERSION_IS_API_COMPATIBLE(0, 2, 0)
+static void toxav_callback_bit_rate_status(ToxAV *av,
+        void *callback, void *user_data)
+{
+    // dummy function
+}
+
+#define TOXAV_ERR_BIT_RATE_SET_INVALID_AUDIO_BIT_RATE (TOXAV_ERR_BIT_RATE_SET_INVALID_BIT_RATE)
+#define TOXAV_ERR_BIT_RATE_SET_INVALID_VIDEO_BIT_RATE (TOXAV_ERR_BIT_RATE_SET_INVALID_BIT_RATE)
+
+static bool toxav_bit_rate_set(ToxAV *av, uint32_t friend_number, int32_t audio_bit_rate,
+                               int32_t video_bit_rate, TOXAV_ERR_BIT_RATE_SET *error)
+{
+    bool res = toxav_video_set_bit_rate(av, friend_number, video_bit_rate, error);
+
+    if (*error == TOXAV_ERR_BIT_RATE_SET_INVALID_BIT_RATE)
+    {
+        *error = TOXAV_ERR_BIT_RATE_SET_INVALID_VIDEO_BIT_RATE;
+    }
+
+    return res;
+}
+#else
+// no need to fake the function
+#endif
+
+#endif
+/*
+ * ------------------------------------------------------------
+ * TOXCORE compatibility layer --------------------------------
+ * ------------------------------------------------------------
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
 #define c_sleep(x) usleep(1000*x)
 
