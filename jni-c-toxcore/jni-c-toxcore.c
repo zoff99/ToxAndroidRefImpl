d0b53d7c23578cd6183ac4c2ea261e90c8a93c12
zoff99 <zoff@zoff.cc>
2018-11-11 14:51:21 +0100

    turn OFF resumable FTs for now

--- jni-c-toxcore/jni-c-toxcore.c
+++ jni-c-toxcore/jni-c-toxcore.c
@@ -2114,7 +2114,12 @@ void Java_com_zoffcc_applications_trifa_MainActivity_init__real(JNIEnv *env, job
     // -------- _callbacks_ --------
     start_filter_audio(recording_samling_rate);
     set_delay_ms_filter_audio(10, global_audio_frame_duration_ms);
-    tox_set_filetransfer_resumable(true);
+
+    // -------- resumable FTs: not working fully yet, so turn it off --------
+    // tox_set_filetransfer_resumable(true);
+    tox_set_filetransfer_resumable(false);
+    // -------- resumable FTs: not working fully yet, so turn it off --------
+
     // ----------- create Tox instance -----------
     const char *proxy_host_str = (*env)->GetStringUTFChars(env, proxy_host, NULL);
     tox_global = create_tox((int)udp_enabled, (int)orbot_enabled, (const char *)proxy_host_str, (uint16_t)proxy_port,
