19caec35fa056f1ecd4c4dbcc6e68779147e6923
zoff99 <zoff@zoff.cc>
2019-07-26 19:50:04 +0200

    CI build script

--- circle_scripts/trifa.sh
+++ circle_scripts/trifa.sh
@@ -206,6 +206,7 @@ echo "##########################################"
 
 # copy the source ----------
 cp -av /root/work/android-refimpl-app $_s_/trifa_src/
+cp -av /root/work/jni-c-toxcore $_s_/trifa_src/
 # copy JNI libs ------------
 cp -av /root/work//artefacts//android/libs/armeabi/libjni-c-toxcore.so $_s_/trifa_src/android-refimpl-app/app/nativelibs/armeabi-v7a/
 cp -av /root/work//artefacts//android/libs/x86/libjni-c-toxcore.so $_s_/trifa_src/android-refimpl-app/app/nativelibs/x86/
@@ -253,8 +254,18 @@ if [ "$CIRCLE_BRANCH""x" == "zoff99/maven_artefactx" ]; then
     cd $_s_/trifa_src/android-refimpl-app/
     ls -al ./gradlew
 
-    if [[ "$current_tag""x"  =~ ^trifajni-.* ]] ; then echo aaa ;fi
+    if [[ "$current_tag""x"  =~ ^trifajni-.* ]] ; then
+        echo "############### ------------ ###################"
+        echo "############### ------------ ###################"
+        echo "trying to upload artefact to bintray ..."
+        echo "############### ------------ ###################"
+        echo "############### ------------ ###################"
         ./gradlew :jnilib:bintrayUpload --info || exit 1
+        echo "############### ------------ ###################"
+        echo "############### ------------ ###################"
+        echo "trying to upload artefact to bintray ... DONE"
+        echo "############### ------------ ###################"
+        echo "############### ------------ ###################"
     fi
 
     find ~/.m2/repository -type f -exec ls -al {} \;
