db4b987c5c42d9977e33fb8d8c2f6769abaa87a8
zoff99 <zoff@zoff.cc>
2020-01-12 14:11:53 +0100

    higher video encoding rate at maximum

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/TRIFAGlobals.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/TRIFAGlobals.java
@@ -101,10 +101,9 @@ public class TRIFAGlobals
     final static int VIDEO_ENCODER_MAX_QUANTIZER_HIGH = 10;
 
     final static int VIDEO_ENCODER_MAX_BITRATE_LOW = 120;
-    final static int VIDEO_ENCODER_MAX_BITRATE_MED = 200;
-    final static int VIDEO_ENCODER_MAX_BITRATE_HIGH = 900;
+    final static int VIDEO_ENCODER_MAX_BITRATE_MED = 250;
+    final static int VIDEO_ENCODER_MAX_BITRATE_HIGH = 1800;
 
-    final static int GLOBAL_MAX_VIDEO_BITRATE = 1600;
     final static int GLOBAL_MIN_VIDEO_BITRATE = 100;
     final static int GLOBAL_MIN_AUDIO_BITRATE = 6; // allowed values: (xx>=6) && (xx<=510)
 
