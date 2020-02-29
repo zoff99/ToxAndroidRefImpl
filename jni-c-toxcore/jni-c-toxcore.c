786d244001dd308e65dea492eb542c1746487b87
zoff99 <zoff@zoff.cc>
2020-02-29 14:04:47 +0100

    new version (missed it before the maven artefact release)

--- jni-c-toxcore/jni-c-toxcore.c
+++ jni-c-toxcore/jni-c-toxcore.c
@@ -77,8 +77,8 @@
 // ----------- version -----------
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 99
-#define VERSION_PATCH 35
-static const char global_version_string[] = "0.99.35";
+#define VERSION_PATCH 36
+static const char global_version_string[] = "0.99.36";
 // ----------- version -----------
 // ----------- version -----------
 
