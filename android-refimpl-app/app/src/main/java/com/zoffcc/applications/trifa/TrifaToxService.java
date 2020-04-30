7b8d993bdf2908c5bfe1dd7a957567384adb4c53
zoff99 <zoff@zoff.cc>
2020-04-30 21:52:11 +0200

    iterate even faster for group audio

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/TrifaToxService.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/TrifaToxService.java
@@ -1405,7 +1405,7 @@ public class TrifaToxService extends Service
                     {
                         if (Callstate.audio_group_active)
                         {
-                            tox_iteration_interval_ms = 3; // if we are in a group audio call iterate more often
+                            tox_iteration_interval_ms = 2; // if we are in a group audio call iterate more often
                         }
                         else
                         {
