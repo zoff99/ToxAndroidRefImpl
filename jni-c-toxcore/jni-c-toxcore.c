3d03b29f826e2fcf6e2417503f02588a3e680330
zoff99 <zoff@zoff.cc>
2020-06-19 21:03:54 +0200

    new JNI version

--- jni-c-toxcore/jni-c-toxcore.c
+++ jni-c-toxcore/jni-c-toxcore.c
@@ -77,8 +77,8 @@
 // ----------- version -----------
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 99
-#define VERSION_PATCH 43
-static const char global_version_string[] = "0.99.43";
+#define VERSION_PATCH 44
+static const char global_version_string[] = "0.99.44";
 // ----------- version -----------
 // ----------- version -----------
 
