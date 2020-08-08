19db76b02b2872a146e6e681c006fc2c831a7334
zoff99 <zoff@zoff.cc>
2020-08-08 13:15:38 +0200

    WIP: tests

--- android-refimpl-app/app/src/androidTest/java/com/zoffcc/applications/trifa/StartMainActivityWrapperTest.java
+++ android-refimpl-app/app/src/androidTest/java/com/zoffcc/applications/trifa/StartMainActivityWrapperTest.java
@@ -4,28 +4,45 @@ import android.util.Log;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.filters.RequiresDevice;
 
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class StartMainActivityWrapperTest
 {
 
     private static final String TAG = "TEST001";
 
+    @Rule
+    public ActivityScenarioRule<StartMainActivityWrapper> rule = new ActivityScenarioRule<>(
+            StartMainActivityWrapper.class);
+
+    @Test
+    public void Test_Startup()
+    {
+        Log.i(TAG, "Test_Startup");
+        ActivityScenario<StartMainActivityWrapper> scenario = rule.getScenario();
+        // Your test code goes here.
+    }
+
     @Before
     public void setUp() throws Exception
     {
+        Log.i(TAG, "setUp");
     }
 
     @After
     public void tearDown() throws Exception
     {
-    }
-
-    @Test
-    public void onCreate()
-    {
-        Log.i(TAG, "onCreate");
+        Log.i(TAG, "tearDown");
     }
 }
