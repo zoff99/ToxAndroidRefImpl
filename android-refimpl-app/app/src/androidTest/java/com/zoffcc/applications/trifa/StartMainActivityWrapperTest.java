432d4b975bff8db48cb15c447c541b1be3e6a885
zoff99 <zoff@zoff.cc>
2020-08-08 12:52:07 +0200

    WIP: some tests

---
+++ android-refimpl-app/app/src/androidTest/java/com/zoffcc/applications/trifa/StartMainActivityWrapperTest.java
@@ -0,0 +1,31 @@
+package com.zoffcc.applications.trifa;
+
+import android.util.Log;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StartMainActivityWrapperTest
+{
+
+    private static final String TAG = "TEST001";
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void onCreate()
+    {
+        Log.i(TAG, "onCreate");
+    }
+}
