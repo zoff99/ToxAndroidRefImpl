e9946b294ca0c14d3833aa12b841404e1675f7d8
zoff99 <zoff@zoff.cc>
2020-11-15 14:07:20 +0100

    circle CI broken checkout fix

---
+++ .circleci/checkout_fix.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+set -e
+
+# Workaround old docker images with incorrect $HOME
+# check https://github.com/docker/docker/issues/2968 for details
+if [ "${HOME}" = "/" ]
+then
+  export HOME=$(getent passwd $(id -un) | cut -d: -f6)
+fi
+
+echo Using SSH Config Dir $SSH_CONFIG_DIR
+
+mkdir -p $SSH_CONFIG_DIR
+
+echo 'github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+bitbucket.org ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAubiN81eDcafrgMeLzaFPsw2kNvEcqTKl/VqLat/MaB33pZy0y3rJZtnqwR2qOOvbwKZYKiEO1O6VqNEBxKvJJelCq0dTXWT5pbO2gDXC6h6QDXCaHo6pOHGPUy+YBaGQRGuSusMEASYiWunYN0vCAI8QaXnWMXNMdFP3jHAJH0eDsoiGnLPBlBp4TNm6rYI74nMzgz3B9IikW4WVK+dc8KZJZWYjAuORU3jc1c/NPskD2ASinf8v3xnfXeukU0sJ5N6m5E8VLjObPEO+mN2t/FZTMZLiFqPWc/ALSqnMnnhwrNi2rbfg/rd/IpL8Le3pSBne8+seeFVBoGqzHM9yXw==
+' >> $SSH_CONFIG_DIR/known_hosts
+
+(umask 077; touch $SSH_CONFIG_DIR/id_rsa)
+chmod 0600 $SSH_CONFIG_DIR/id_rsa
+(cat <<EOF > $SSH_CONFIG_DIR/id_rsa
+$CHECKOUT_KEY
+EOF
+)
+
+export GIT_SSH_COMMAND='ssh -i $SSH_CONFIG_DIR/id_rsa -o UserKnownHostsFile=$SSH_CONFIG_DIR/known_hosts'
+
+# use git+ssh instead of https
+# --> broken # git config --global url."ssh://git@github.com".insteadOf "https://github.com" || true
+git config --global gc.auto 0 || true
+
+if [ -e /root/work/.git ]
+then
+  cd /root/work
+  git remote set-url origin "$CIRCLE_REPOSITORY_URL" || true
+else
+  mkdir -p /root/work
+  cd /root/work
+  git clone "$CIRCLE_REPOSITORY_URL" .
+fi
+
+if [ -n "$CIRCLE_TAG" ]
+then
+  git fetch --force origin "refs/tags/${CIRCLE_TAG}"
+else
+  git fetch --force origin 'zoff99/dev003:remotes/origin/zoff99/dev003'
+fi
+
+
+if [ -n "$CIRCLE_TAG" ]
+then
+  git reset --hard "$CIRCLE_SHA1"
+  git checkout -q "$CIRCLE_TAG"
+elif [ -n "$CIRCLE_BRANCH" ]
+then
+  git reset --hard "$CIRCLE_SHA1"
+  git checkout -q -B "$CIRCLE_BRANCH"
+fi
+
+git reset --hard "$CIRCLE_SHA1"
