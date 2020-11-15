e7d5542c1070e5fde2c60bb0367d33b6c9edb72b
zoff99 <zoff@zoff.cc>
2020-11-15 14:27:39 +0100

    CI fix

--- .circleci/checkout_fix.sh
+++ .circleci/checkout_fix.sh
@@ -44,7 +44,7 @@ if [ -n "$CIRCLE_TAG" ]
 then
   git fetch --force origin "refs/tags/${CIRCLE_TAG}"
 else
-  git fetch --force origin "${CIRCLE_BRANCH}:remotes/origin/${CIRCLE_BRANCH}'
+  git fetch --force origin "${CIRCLE_BRANCH}:remotes/origin/${CIRCLE_BRANCH}"
 fi
 
 
