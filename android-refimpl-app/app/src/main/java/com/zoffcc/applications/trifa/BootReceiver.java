463b215bbcedb63d792535c66d84084c8da2a26f
zoff99 <zoff99@users.noreply.github.com>
2021-01-29 21:40:17 +0100

    add start on boot, this does NOT work on Android 10 and up.

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/BootReceiver.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/BootReceiver.java
@@ -22,6 +22,8 @@ package com.zoffcc.applications.trifa;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver
@@ -35,7 +37,28 @@ public class BootReceiver extends BroadcastReceiver
         {
             Log.i(TAG, "-- ON_BOOT -- :" + intent.getAction());
 
-            // see: https://developer.android.com/training/scheduling/alarms.html#java
+            try
+            {
+                SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+                boolean start_on_boot = settings.getBoolean("start_on_boot", false);
+
+                if (start_on_boot)
+                {
+                    // TODO: this is not working anymore starting with Android 10
+                    // https://developer.android.com/guide/components/activities/background-starts
+                    // thanks Google
+
+                    Log.i(TAG, "-- ON_BOOT -- :starting trifa ...");
+                    Intent i = new Intent(context, StartMainActivityWrapper.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(i);
+                    Log.i(TAG, "-- ON_BOOT -- :activity started");
+                }
+            }
+            catch (Exception e)
+            {
+                Log.i(TAG, "-- ON_BOOT -- :EE01:" + e.getMessage());
+            }
         }
     }
 }
