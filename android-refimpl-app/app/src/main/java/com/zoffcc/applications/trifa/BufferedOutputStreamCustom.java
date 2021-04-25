13cf036d20b0e550ed2ad69ac9ccaa1111cb8d3a
zoff99 <zoff99@users.noreply.github.com>
2021-04-25 21:37:41 +0200

    correct date in license

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/BufferedOutputStreamCustom.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/BufferedOutputStreamCustom.java
@@ -1,6 +1,6 @@
 /**
  * [TRIfA], Java part of Tox Reference Implementation for Android
- * Copyright (C) 2017 Zoff <zoff@zoff.cc>
+ * Copyright (C) 2021 Zoff <zoff@zoff.cc>
  * <p>
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
