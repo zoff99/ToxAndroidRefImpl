0e15e6383437a371a3be206cbfbedbec1a5221b4
zoff99 <zoff99@users.noreply.github.com>
2021-10-03 16:03:19 +0200

    fix mimetype check

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/MessageListHolder_file_outgoing_state_resume.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/MessageListHolder_file_outgoing_state_resume.java
@@ -254,7 +254,7 @@ public class MessageListHolder_file_outgoing_state_resume extends RecyclerView.V
                 mimeType = URLConnection.guessContentTypeFromName(message.filename_fullpath.toLowerCase());
             }
 
-            if (mimeType.startsWith("image"))
+            if (mimeType.startsWith("image/"))
             {
                 is_image = true;
             }
