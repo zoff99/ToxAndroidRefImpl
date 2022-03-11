a1e75f4ed638eaf8f57961fbeca083ab6429ec48
zoff99 <zoff99@users.noreply.github.com>
2022-03-11 14:26:55 +0100

    correct tag name

--- tools/maven_jniartefact_prepare_new_release_version.sh
+++ tools/maven_jniartefact_prepare_new_release_version.sh
@@ -43,8 +43,8 @@ sed -i -e 's#maven_artefact_int_version".*#maven_artefact_int_version", '"$next_
 sed -i -e 's#maven_artefact_version = .*#maven_artefact_version = '"'""$next_m_version""'"'#' "$f1"
 
 commit_message="jni ""$next_m_version"
-tag_name="trifajni-$next_m_version"
+tag_name="trifajni-""$next_m_version"
 
 git commit -m "$commit_message" "$f1"
-echo git tag -a "$next_m_version" -m "$next_m_version"
+git tag -a "$next_m_version" -m "$tag_name"
 
