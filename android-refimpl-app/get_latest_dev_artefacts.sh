b362b8c8869760af8360aafb09113cea20ddb709
zoff99 <zoff99@users.noreply.github.com>
2022-04-26 17:24:48 +0200

    fix artefact download script.

--- android-refimpl-app/get_latest_dev_artefacts.sh
+++ android-refimpl-app/get_latest_dev_artefacts.sh
@@ -5,10 +5,15 @@ export _HOME2_
 _HOME_=$(cd $_HOME2_;pwd)
 export _HOME_
 
-url_arm_lib='https://circleci.com/api/v1.1/project/github/zoff99/ToxAndroidRefImpl/latest/artifacts/0/artefacts/android/libs/armeabi/libjni-c-toxcore.so?filter=successful&branch=zoff99%2Fdev003'
-url_arm64_lib='https://circleci.com/api/v1.1/project/github/zoff99/ToxAndroidRefImpl/latest/artifacts/0/artefacts/android/libs/arm64-v8a/libjni-c-toxcore.so?filter=successful&branch=zoff99%2Fdev003'
-url_x86_lib='https://circleci.com/api/v1.1/project/github/zoff99/ToxAndroidRefImpl/latest/artifacts/0/artefacts/android/libs/x86/libjni-c-toxcore.so?filter=successful&branch=zoff99%2Fdev003'
-url_x86_64_lib='https://circleci.com/api/v1.1/project/github/zoff99/ToxAndroidRefImpl/latest/artifacts/0/artefacts/android/libs/x86_64/libjni-c-toxcore.so?filter=successful&branch=zoff99%2Fdev003'
+jq --version || exit 1
+
+furl_text=$(wget -q -O - 'https://circleci.com/api/v1.1/project/github/zoff99/ToxAndroidRefImpl/latest/artifacts?branch=zoff99%2Fdev003&filter=successful')
+libs_urls=$(echo $furl_text |jq ' .[].url' |grep 'libjni-c-toxcore.so')
+
+url_arm_lib=$(echo "$libs_urls" |grep /armeabi/|tr -d '"')
+url_arm64_lib=$(echo "$libs_urls" |grep /arm64-v8a/|tr -d '"')
+url_x86_lib=$(echo "$libs_urls" |grep /x86/|tr -d '"')
+url_x86_64_lib=$(echo "$libs_urls" |grep /x86_64/|tr -d '"')
 
 rm -f "$_HOME_""/app/nativelibs/armeabi-v7a/libjni-c-toxcore.so"
 rm -f "$_HOME_""/app/nativelibs/arm64-v8a/libjni-c-toxcore.so"
