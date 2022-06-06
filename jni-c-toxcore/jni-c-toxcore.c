e91251a6b3edf6e6b7b1aba79fb927823b5aeff8
zoff99 <zoff99@users.noreply.github.com>
2022-06-06 13:06:12 +0200

    increase version

--- jni-c-toxcore/jni-c-toxcore.c
+++ jni-c-toxcore/jni-c-toxcore.c
@@ -85,8 +85,8 @@
 // ----------- version -----------
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 99
-#define VERSION_PATCH 80
-static const char global_version_string[] = "0.99.80";
+#define VERSION_PATCH 81
+static const char global_version_string[] = "0.99.81";
 // ----------- version -----------
 // ----------- version -----------
 
