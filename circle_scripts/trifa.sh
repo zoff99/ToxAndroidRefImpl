90a2715a834c77a5e23e240cddf0bb4de0980625
zoff99 <zoff99@users.noreply.github.com>
2022-06-06 16:19:48 +0200

    tweak CI

--- circle_scripts/trifa.sh
+++ circle_scripts/trifa.sh
@@ -221,26 +221,21 @@ echo "##########################################"
 # copy the source ----------
 cp -av /root/work/android-refimpl-app $_s_/trifa_src/
 cp -av /root/work/jni-c-toxcore $_s_/trifa_src/
-# copy JNI libs ------------
-cp -av /root/work//artefacts//android/libs/armeabi/libjni-c-toxcore.so $_s_/trifa_src/android-refimpl-app/app/nativelibs/armeabi-v7a/
-cp -av /root/work//artefacts//android/libs/arm64-v8a/libjni-c-toxcore.so $_s_/trifa_src/android-refimpl-app/app/nativelibs/arm64-v8a/
-cp -av /root/work//artefacts//android/libs/x86/libjni-c-toxcore.so $_s_/trifa_src/android-refimpl-app/app/nativelibs/x86/
-cp -av /root/work//artefacts//android/libs/x86_64/libjni-c-toxcore.so $_s_/trifa_src/android-refimpl-app/app/nativelibs/x86_64/
 
 ls -hal /root/work//artefacts//android/libs/armeabi/libjni-c-toxcore.so || exit 1
 ls -hal /root/work//artefacts//android/libs/arm64-v8a/libjni-c-toxcore.so || exit 1
 ls -hal /root/work//artefacts//android/libs/x86/libjni-c-toxcore.so || exit 1
 ls -hal /root/work//artefacts//android/libs/x86_64/libjni-c-toxcore.so || exit 1
 
-echo "###### ---------- ARM --------------------"
-scanelf -qT $_s_/trifa_src/android-refimpl-app/app/nativelibs/armeabi-v7a//libjni-c-toxcore.so
-echo "###### ---------- ARM64 --------------------"
-scanelf -qT $_s_/trifa_src/android-refimpl-app/app/nativelibs/arm64-v8a//libjni-c-toxcore.so
-echo "###### ---------- X86 --------------------"
-scanelf -qT $_s_/trifa_src/android-refimpl-app/app/nativelibs/x86//libjni-c-toxcore.so
-echo "###### ---------- X86_64 --------------------"
-scanelf -qT $_s_/trifa_src/android-refimpl-app/app/nativelibs/x86_64//libjni-c-toxcore.so
-echo "###### ------------------------------"
+#echo "###### ---------- ARM --------------------"
+#scanelf -qT $_s_/trifa_src/android-refimpl-app/app/nativelibs/armeabi-v7a//libjni-c-toxcore.so
+#echo "###### ---------- ARM64 --------------------"
+#scanelf -qT $_s_/trifa_src/android-refimpl-app/app/nativelibs/arm64-v8a//libjni-c-toxcore.so
+#echo "###### ---------- X86 --------------------"
+#scanelf -qT $_s_/trifa_src/android-refimpl-app/app/nativelibs/x86//libjni-c-toxcore.so
+#echo "###### ---------- X86_64 --------------------"
+#scanelf -qT $_s_/trifa_src/android-refimpl-app/app/nativelibs/x86_64//libjni-c-toxcore.so
+#echo "###### ------------------------------"
 
 # ----- get the source -----
 
@@ -270,141 +265,68 @@ cd $_s_/trifa_src/android-refimpl-app/
 pwd
 ls -al
 chmod a+rx ./gradlew
-if [ "$CIRCLE_BRANCH""x" == "zoff99/maven_artefactx" ]; then
-    echo "not building TRIfA app! -> maven branch"
-    :
-else
-    echo "Building TRIfA app"
-    ./gradlew :app:dependencies || echo "OK"
-    # ./gradlew assembleDebug # this is a workaround to build problems, not sure how to fix it otherwise
-    ./gradlew :app:build --max-workers=1 --stacktrace --no-daemon || ./gradlew :app:build --stacktrace --no-daemon # first build may FAIL
-fi
 # --------- GRADLE - build app -------------
 
 
-
-
-
-
 # --------- bintray artefact -------------
-if [ "$CIRCLE_BRANCH""x" == "zoff99/maven_artefactx" ]; then
-    cd $_s_/trifa_src/jni-c-toxcore/; mkdir -p ../android-refimpl-app/jnilib/src/main/jniLibs/armeabi-v7a/
-    cd $_s_/trifa_src/jni-c-toxcore/; cp -av /root/work//artefacts//android/libs/armeabi/libjni-c-toxcore.so ../android-refimpl-app/jnilib/src/main/jniLibs/armeabi-v7a/
+cd $_s_/trifa_src/jni-c-toxcore/; mkdir -p ../android-refimpl-app/jnilib/src/main/jniLibs/armeabi-v7a/
+cd $_s_/trifa_src/jni-c-toxcore/; cp -av /root/work//artefacts//android/libs/armeabi/libjni-c-toxcore.so ../android-refimpl-app/jnilib/src/main/jniLibs/armeabi-v7a/
 
-    cd $_s_/trifa_src/jni-c-toxcore/; mkdir -p ../android-refimpl-app/jnilib/src/main/jniLibs/arm64-v8a/
-    cd $_s_/trifa_src/jni-c-toxcore/; cp -av /root/work//artefacts//android/libs/arm64-v8a/libjni-c-toxcore.so ../android-refimpl-app/jnilib/src/main/jniLibs/arm64-v8a/
+cd $_s_/trifa_src/jni-c-toxcore/; mkdir -p ../android-refimpl-app/jnilib/src/main/jniLibs/arm64-v8a/
+cd $_s_/trifa_src/jni-c-toxcore/; cp -av /root/work//artefacts//android/libs/arm64-v8a/libjni-c-toxcore.so ../android-refimpl-app/jnilib/src/main/jniLibs/arm64-v8a/
 
-    cd $_s_/trifa_src/jni-c-toxcore/; mkdir -p ../android-refimpl-app/jnilib/src/main/jniLibs/x86/
-    cd $_s_/trifa_src/jni-c-toxcore/; cp -av /root/work//artefacts//android/libs/x86/libjni-c-toxcore.so ../android-refimpl-app/jnilib/src/main/jniLibs/x86/
+cd $_s_/trifa_src/jni-c-toxcore/; mkdir -p ../android-refimpl-app/jnilib/src/main/jniLibs/x86/
+cd $_s_/trifa_src/jni-c-toxcore/; cp -av /root/work//artefacts//android/libs/x86/libjni-c-toxcore.so ../android-refimpl-app/jnilib/src/main/jniLibs/x86/
 
-    cd $_s_/trifa_src/jni-c-toxcore/; mkdir -p ../android-refimpl-app/jnilib/src/main/jniLibs/x86_64/
-    cd $_s_/trifa_src/jni-c-toxcore/; cp -av /root/work//artefacts//android/libs/x86_64/libjni-c-toxcore.so ../android-refimpl-app/jnilib/src/main/jniLibs/x86_64/
+cd $_s_/trifa_src/jni-c-toxcore/; mkdir -p ../android-refimpl-app/jnilib/src/main/jniLibs/x86_64/
+cd $_s_/trifa_src/jni-c-toxcore/; cp -av /root/work//artefacts//android/libs/x86_64/libjni-c-toxcore.so ../android-refimpl-app/jnilib/src/main/jniLibs/x86_64/
 
-    cd $_s_/trifa_src/android-refimpl-app/ ; ./gradlew :jnilib:dependencies
-    cd $_s_/trifa_src/android-refimpl-app/ ; ./gradlew :jnilib:build --max-workers=1 --stacktrace --no-daemon || ./gradlew :jnilib:build --stacktrace --no-daemon # first build may FAIL
-    cd $_s_/trifa_src/android-refimpl-app/ ; ./gradlew :jnilib:install --info
-    cd $_s_/trifa_src/android-refimpl-app/
-    ls -al ./gradlew
+cd $_s_/trifa_src/android-refimpl-app/ ; ./gradlew :jnilib:dependencies
+cd $_s_/trifa_src/android-refimpl-app/ ; ./gradlew :jnilib:build --max-workers=1 --stacktrace --no-daemon || ./gradlew :jnilib:build --stacktrace --no-daemon # first build may FAIL
+cd $_s_/trifa_src/android-refimpl-app/ ; ./gradlew :jnilib:install --info
+cd $_s_/trifa_src/android-refimpl-app/
+ls -al ./gradlew
 
-    if [[ "$current_git_tag""x"  =~ ^trifajni-.* ]] ; then
-        echo "############### ------------ ###################"
-        echo "############### ------------ ###################"
-        echo "trying to upload artefact to bintray ..."
-        echo "############### ------------ ###################"
-        echo "############### ------------ ###################"
-        ./gradlew :jnilib:bintrayUpload --info || exit 1
-        echo "############### ------------ ###################"
-        echo "############### ------------ ###################"
-        echo "trying to upload artefact to bintray ... DONE"
-        echo "############### ------------ ###################"
-        echo "############### ------------ ###################"
-    else
-        echo "not uploading artefact to bintray!! -> not maven branch"
-    fi
+if [[ "$current_git_tag""x"  =~ ^trifajni-.* ]] ; then
+    echo "############### ------------ ###################"
+    echo "############### ------------ ###################"
+    echo "trying to upload artefact to bintray ..."
+    echo "############### ------------ ###################"
+    echo "############### ------------ ###################"
+    ./gradlew :jnilib:bintrayUpload --info
+    echo "############### ------------ ###################"
+    echo "############### ------------ ###################"
+    echo "trying to upload artefact to bintray ... DONE"
+    echo "############### ------------ ###################"
+    echo "############### ------------ ###################"
+else
+    echo "not uploading artefact to bintray!! -> not maven branch"
+fi
 
-    find ~/.m2/repository -type f -exec ls -al {} \;
+find ~/.m2/repository -type f -exec ls -al {} \;
 # --------- bintray artefact -------------
 # --------- show generated aar file -----------
-    cd $_s_/trifa_src/android-refimpl-app/ ; ls -al jnilib/build/outputs/aar/
-    cd ~ ; find ./ -name '*.aar' | grep 'trifa-jni'
+cd $_s_/trifa_src/android-refimpl-app/ ; ls -al jnilib/build/outputs/aar/
+cd ~ ; find ./ -name '*.aar' | grep 'trifa-jni'
 
-    unzip -t ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/1.*/trifa-jni-lib-1.*.aar
-    sha256sum ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/1.*/trifa-jni-lib-1.*.aar
-    cat ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/1.*/trifa-jni-lib-1.*.pom
-    cat ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/maven-metadata-local.xml
+unzip -t ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/1.*/trifa-jni-lib-1.*.aar
+sha256sum ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/1.*/trifa-jni-lib-1.*.aar
+cat ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/1.*/trifa-jni-lib-1.*.pom
+cat ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/maven-metadata-local.xml
 
-    zip -r $CIRCLE_ARTIFACTS/local_maven.zip ~/.m2
-    zip -r $CIRCLE_ARTIFACTS/local_maven_trifa_jni_"$latest_git_tag".zip ~/.m2
+zip -r $CIRCLE_ARTIFACTS/local_maven.zip ~/.m2
+zip -r $CIRCLE_ARTIFACTS/local_maven_trifa_jni_"$latest_git_tag".zip ~/.m2
 
-    cd $_s_/trifa_src/android-refimpl-app/ ; unzip -t ./jnilib/build/outputs/aar/trifa-jni-lib-release.aar
-    cd $_s_/trifa_src/android-refimpl-app/ ; sha256sum ./jnilib/build/outputs/aar/trifa-jni-lib-release.aar
-    cd $_s_/trifa_src/android-refimpl-app/ ; cp -av ./jnilib/build/outputs/aar/trifa-jni-lib-release.aar $CIRCLE_ARTIFACTS/
-    cd $_s_/trifa_src/android-refimpl-app/ ; ls -hal ./jnilib/build/outputs/aar/trifa-jni-lib-release.aar || exit 1
+cd $_s_/trifa_src/android-refimpl-app/ ; unzip -t ./jnilib/build/outputs/aar/trifa-jni-lib-release.aar
+cd $_s_/trifa_src/android-refimpl-app/ ; sha256sum ./jnilib/build/outputs/aar/trifa-jni-lib-release.aar
+cd $_s_/trifa_src/android-refimpl-app/ ; cp -av ./jnilib/build/outputs/aar/trifa-jni-lib-release.aar $CIRCLE_ARTIFACTS/
+cd $_s_/trifa_src/android-refimpl-app/ ; ls -hal ./jnilib/build/outputs/aar/trifa-jni-lib-release.aar || exit 1
 
-    # cp -av ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/1.*/trifa-jni-lib-1.*.aar $CIRCLE_ARTIFACTS/
-    # ls -hal ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/1.*/trifa-jni-lib-1.*.aar || exit 1
-fi
+# cp -av ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/1.*/trifa-jni-lib-1.*.aar $CIRCLE_ARTIFACTS/
+# ls -hal ~/.m2/repository/com/zoffcc/applications/trifajni/trifa-jni-lib/1.*/trifa-jni-lib-1.*.aar || exit 1
+
 # --------- show generated aar file -----------
 
-
-
-
-
-
-if [ "$CIRCLE_BRANCH""x" != "zoff99/maven_artefactx" ]; then
-
-    # ----------- show generated apk file -----------
-    cd $_s_/trifa_src/android-refimpl-app/
-    ls -al app/build/outputs/apk/
-    find ./ -name '*.apk'
-    mkdir -p app/build/outputs/apk/
-    cp -av ./app/build/outputs/apk/release/app-release-unsigned.apk ./app/build/outputs/apk/
-
-
-    ls -hal $_s_/trifa_src/android-refimpl-app/app/build/outputs/apk/app-release-unsigned.apk
-    # ----------- show generated apk file -----------
-
-
-
-    zip -d $_s_/trifa_src/android-refimpl-app/app/build/outputs/apk/app-release-unsigned.apk META-INF/\*     # remove signature !!
-    cp -av $_s_/trifa_src/android-refimpl-app/app/build/outputs/apk/app-release-unsigned.apk ~/app.apk
-    cd ~/
-
-    ls -al ~/
-    jarsigner -verbose -keystore ~/.android/debug.keystore -storepass android -keypass android -sigalg SHA1withRSA -digestalg SHA1 -sigfile CERT -signedjar app-signed.apk app.apk androiddebugkey
-    type -a apksigner
-    type -a jarsigner
-    # apksigner sign --ks ~/.android/debug.keystore --ks-key-alias androiddebugkey --out app-signed.apk app.apk
-
-
-    ls -al ~/
-    find . -name zipalign
-    ls -al ./work/trifa_inst/sdk/build-tools/23.0.3/zipalign
-    ls -al $_SDK_/build-tools/23.0.3/zipalign
-    file $_SDK_/build-tools/23.0.3/zipalign
-    ls -al $_SDK_/build-tools/27.0.3/zipalign
-    file $_SDK_/build-tools/27.0.3/zipalign
-    cd ~/
-    # HINT: zipalign is a 32bit binary?
-    $_SDK_/build-tools/27.0.3/zipalign -v 4 ~/app-signed.apk ~/app-signed-aligned.apk
-
-    ls -al ~/
-    pwd
-
-    ls -al
-    cp -av app-signed-aligned.apk $CIRCLE_ARTIFACTS/${CIRCLE_PROJECT_REPONAME}.apk
-
-    ls -hal $CIRCLE_ARTIFACTS/${CIRCLE_PROJECT_REPONAME}.apk || exit 1
-
-
-    ##   also make apk files with different names for each build (for individual downloads)
-    cp -av $CIRCLE_ARTIFACTS/${CIRCLE_PROJECT_REPONAME}.apk $CIRCLE_ARTIFACTS/${CIRCLE_PROJECT_REPONAME}_circleci_$CIRCLE_SHA1.apk
-    ##   qr code to scan with your phone to directly download the apk file (for convenience)
-    qrencode -o $CIRCLE_ARTIFACTS/QR_apk.png 'https://circle-artifacts.com/gh/'${CIRCLE_PROJECT_USERNAME}'/'${CIRCLE_PROJECT_REPONAME}'/'${CIRCLE_BUILD_NUM}'/artifacts/'${CIRCLE_NODE_INDEX}'/tmp/'`basename $CIRCLE_ARTIFACTS`'/'"${CIRCLE_PROJECT_REPONAME}_circleci_$CIRCLE_SHA1.apk" ; exit 0
-    ##   qr code to go directly to the aritfacts (to scan with phone)
-    qrencode -o $CIRCLE_ARTIFACTS/QR_artifacts.png 'https://circleci.com/gh/'${CIRCLE_PROJECT_USERNAME}'/'${CIRCLE_PROJECT_REPONAME}'/'${CIRCLE_BUILD_NUM}'#artifacts' ; exit 0
-
-fi
 
 
 pwd
