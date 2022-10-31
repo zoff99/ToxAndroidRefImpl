425a297bdf3e3ad1b7f7224179432e9768a8bf0e
zoff99 <zoff99@users.noreply.github.com>
2022-10-31 21:56:55 +0100

    new API functions.

--- jni-c-toxcore/jni-c-toxcore.c
+++ jni-c-toxcore/jni-c-toxcore.c
@@ -3227,10 +3227,39 @@ Java_com_zoffcc_applications_trifa_MainActivity_bootstrap_1single(JNIEnv *env, j
 // -----------------
 // -----------------
 
+JNIEXPORT jstring JNICALL
+Java_com_zoffcc_applications_trifa_MainActivity_tox_1get_1all_1tcp_1relays(JNIEnv *env, jobject thiz)
+{
+    size_t length = 60301; // minimum according to tox.h
+    char result_c[length];
+    CLEAR(result_c);
 
+    if(tox_global == NULL)
+    {
+        return (jstring)NULL;
+    }
 
+    tox_get_all_tcp_relays(tox_global, (uint8_t *)result_c);
+    jstring result = c_safe_string_from_java((char *)result_c, length);
+    return result;
+}
 
+JNIEXPORT jstring JNICALL
+Java_com_zoffcc_applications_trifa_MainActivity_tox_1get_1all_1udp_1connections(JNIEnv *env, jobject thiz)
+{
+    size_t length = 60301; // minimum according to tox.h
+    char result_c[length];
+    CLEAR(result_c);
 
+    if(tox_global == NULL)
+    {
+        return (jstring)NULL;
+    }
+
+    tox_get_all_udp_connections(tox_global, (uint8_t *)result_c);
+    jstring result = c_safe_string_from_java((char *)result_c, length);
+    return result;
+}
 
 JNIEXPORT jstring JNICALL
 Java_com_zoffcc_applications_trifa_MainActivity_get_1my_1toxid(JNIEnv *env, jobject thiz)
