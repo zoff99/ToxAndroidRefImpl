42f0a4085773d1ea28a28a05738f6a5a7968af28
zoff99 <zoff99@users.noreply.github.com>
2022-11-01 12:52:51 +0100

    new JNI version

--- jni-c-toxcore/jni-c-toxcore.c
+++ jni-c-toxcore/jni-c-toxcore.c
@@ -85,8 +85,8 @@
 // ----------- version -----------
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 99
-#define VERSION_PATCH 83
-static const char global_version_string[] = "0.99.83";
+#define VERSION_PATCH 84
+static const char global_version_string[] = "0.99.84";
 // ----------- version -----------
 // ----------- version -----------
 
