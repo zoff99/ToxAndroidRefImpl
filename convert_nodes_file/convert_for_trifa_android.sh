2e08558acd02e9eb87cd80636420dfcad03e04a0
zoff99 <zoff99@users.noreply.github.com>
2023-02-04 09:52:47 +0100

    also add ipv6 nodes from bootstrap list

--- convert_nodes_file/convert_for_trifa_android.sh
+++ convert_nodes_file/convert_for_trifa_android.sh
@@ -21,136 +21,158 @@
 nodes_file="./nodes.json"
 outfile="./nodes2.java"
 
-wget 'https://nodes.tox.chat/json' -O "$nodes_file"
+# wget 'https://nodes.tox.chat/json' -O "$nodes_file"
 
-nodes_count=$(jq .nodes[] "$nodes_file" |grep ipv4|wc -l)
+nodes_count=$(jq .nodes[] "$nodes_file" | grep ipv4 |wc -l)
 echo "nodes:"$nodes_count
 
-#  "last_ping": 1494344722,
-#  "motd": "tox-bootstrapd",
-#  "version": "2016010100",
-#  "status_tcp": false,
-#  "ipv4": "77.37.142.179",
-#  "ipv6": "-",
-#  "port": 33445,
-#  "tcp_ports": [],
-#  "public_key": "98F5830A426C6BF165F895F04B897AFC4F57331B4BE0561F583C9F323194227B",
-#  "maintainer": "ps",
-#  "location": "RU",
-#  "status_udp": false
 
+nodes_count4=$(jq .nodes[] "$nodes_file" |grep ipv4|grep -v '"NONE",'|wc -l)
+echo "nodes4:"$nodes_count4
+
+nodes_count6=$(jq .nodes[] "$nodes_file" |grep ipv6|grep -v '"-",'|wc -l)
+echo "nodes6:"$nodes_count6
+
+
+rm -f "$outfile"
+
+
+
+### UDP ###
+echo ""
+echo ""
+echo "UDP:"
+echo ""
+echo ""
 
 i=0
 good_nodes_count=0
 while [ $i -lt $nodes_count ]; do
         status_udp=$(jq .nodes["$i"].status_udp "$nodes_file")
         if [ "$status_udp""x" == "true""x" ]; then
+                echo "node:$i"
                 good_nodes_count=$[ $good_nodes_count + 1 ]
-        fi
-        i=$[ $i + 1 ]
-done
 
-echo "good nodes:"$good_nodes_count
+                ipv4=$(jq .nodes["$i"].ipv4 "$nodes_file"|tr -d '"')
+                ipv6=$(jq .nodes["$i"].ipv6 "$nodes_file"|tr -d '"')
+                port=$(jq .nodes["$i"].port "$nodes_file"|tr -d '"')
+                pubkey=$(jq .nodes["$i"].public_key "$nodes_file"|tr -d '"')
 
-rm -f "$outfile"
+                # echo "$ipv6 $ipv4 $port $pubkey"
 
-i=0
-while [ $i -lt $nodes_count ]; do
-        ipv4=$(jq .nodes["$i"].ipv4 "$nodes_file"|tr -d '"')
-        port=$(jq .nodes["$i"].port "$nodes_file"|tr -d '"')
-        pubkey=$(jq .nodes["$i"].public_key "$nodes_file"|tr -d '"')
-        status_udp=$(jq .nodes["$i"].status_udp "$nodes_file"|tr -d '"')
-
-        if [ "$status_udp""x" == "true""x" ]; then
-              if [ "$ipv4""x" != "NONEx" ]; then
-              
                 echo -n 'n = BootstrapNodeEntryDB_(true, num_, "'"$ipv4"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
                 echo -n 'insert_node_into_db_real(n);' >> "$outfile"
                 echo -n 'num_++;' >> "$outfile"
-                echo "node #""$i:"
-                if [ $[ $i + 1 ] != $good_nodes_count ]; then
-                        echo -n '' >> "$outfile"
-                fi
                 echo '' >> "$outfile"
-              fi
-        else
-                :
-                # echo "node #""$i:"
-                # echo "node not OK"
-        fi
 
+                if [ "$ipv6""x" != "-""x" ]; then
+                    if [[ $ipv6 = 2* ]] ; then
+                        echo -n 'n = BootstrapNodeEntryDB_(true, num_, "'"$ipv6"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
+                        echo -n 'insert_node_into_db_real(n);' >> "$outfile"
+                        echo -n 'num_++;' >> "$outfile"
+                    else
+                        # lookup ipv6 address
+                        ipv6_lookup=$(dig AAAA +short "$ipv6" 2>/dev/null|tail -1 2>/dev/null)
+                        if [[ $ipv6_lookup = 2* ]] ; then
+                            echo "ipv6 is $ipv6_lookup"
+                            echo -n 'n = BootstrapNodeEntryDB_(true, num_, "'"$ipv6"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
+                            echo -n 'insert_node_into_db_real(n);' >> "$outfile"
+                            echo -n 'num_++;' >> "$outfile"
+                        else
+                            echo "ipv6 address seems broken $ipv6_lookup"
+                        fi
+                    fi
+                fi
+
+                echo '' >> "$outfile"
+        fi
         i=$[ $i + 1 ]
 done
 
 
+
+
+echo "// ================" >> "$outfile"
+echo "// ================" >> "$outfile"
+
+
+
+
+
 ### TCP ###
+echo ""
+echo ""
+echo "TCP:"
+echo ""
+echo ""
 
 
 i=0
 good_nodes_count=0
-while [ $i -lt $nodes_count ]; do
+while [ $i -lt $nodes_count4 ]; do
         status_tcp=$(jq .nodes["$i"].status_tcp "$nodes_file")
         if [ "$status_tcp""x" == "true""x" ]; then
-                good_nodes_count=$[ $good_nodes_count + 1 ]
+                echo "node:$i"
+                good_nodes_count4=$[ $good_nodes_count + 1 ]
+
+
+                ipv4=$(jq .nodes["$i"].ipv4 "$nodes_file"|tr -d '"')
+                ipv6=$(jq .nodes["$i"].ipv6 "$nodes_file"|tr -d '"')
+                ports=$(jq .nodes["$i"].tcp_ports "$nodes_file"|tr -d '"')
+                pubkey=$(jq .nodes["$i"].public_key "$nodes_file"|tr -d '"')
+                ports_count=$(jq '.nodes['"$i"'].tcp_ports|length' "$nodes_file"|tr -d '"')
+
+                echo "$ports len=$ports_count"
+
+                if [ "$ports_count""x" != "0x" ]; then
+                    for tcp_port_index in $(seq 0 $(("$ports_count"-1)) ) ; do
+                        port=$(jq .nodes["$i"].tcp_ports["$tcp_port_index"] "$nodes_file"|tr -d '"')
+                        echo "port=$port"
+
+
+                        if [ "$port""x" != "x" ]; then
+
+                          if [ "$ipv4""x" != "NONEx" ]; then
+                            echo -n 'n = BootstrapNodeEntryDB_(false, num_, "'"$ipv4"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
+                            echo -n 'insert_node_into_db_real(n);' >> "$outfile"
+                            echo -n 'num_++;' >> "$outfile"
+                            echo "node #""$i:"" -> ""$port"
+                            if [ $[ $i + 1 ] != $good_nodes_count ]; then
+                                    echo -n '' >> "$outfile"
+                            fi
+                            echo '' >> "$outfile"
+                          fi
+
+
+                            if [ "$ipv6""x" != "-""x" ]; then
+                                if [[ $ipv6 = 2* ]] ; then
+                                    echo -n 'n = BootstrapNodeEntryDB_(false, num_, "'"$ipv6"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
+                                    echo -n 'insert_node_into_db_real(n);' >> "$outfile"
+                                    echo -n 'num_++;' >> "$outfile"
+                                else
+                                    # lookup ipv6 address
+                                    ipv6_lookup=$(dig AAAA +short "$ipv6" 2>/dev/null|tail -1 2>/dev/null)
+                                    if [[ $ipv6_lookup = 2* ]] ; then
+                                        echo "ipv6 is $ipv6_lookup"
+                                        echo -n 'n = BootstrapNodeEntryDB_(false, num_, "'"$ipv6"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
+                                        echo -n 'insert_node_into_db_real(n);' >> "$outfile"
+                                        echo -n 'num_++;' >> "$outfile"
+                                    else
+                                        echo "ipv6 address seems broken $ipv6_lookup"
+                                    fi
+                                fi
+                                echo '' >> "$outfile"
+                            fi
+
+
+                        fi
+
+
+
+                    done
+                fi
+
         fi
         i=$[ $i + 1 ]
 done
 
-echo "good nodes:"$good_nodes_count
-
-echo "" >> "$outfile"
-echo "" >> "$outfile"
-
-i=0
-while [ $i -lt $nodes_count ]; do
-        ipv4=$(jq .nodes["$i"].ipv4 "$nodes_file"|tr -d '"')
-
-        ports_count=$(jq '.nodes['"$i"'].tcp_ports|length' "$nodes_file"|tr -d '"')
-        # echo $ports_count
-
-        if [ "$ports_count""x" != "0x" ]; then
-            for tcp_port_index in $(seq 0 $(("$ports_count"-1)) ) ; do
-                port=$(jq .nodes["$i"].tcp_ports["$tcp_port_index"] "$nodes_file"|tr -d '"')
-                # echo "$tcp_port_index"" -> ""$port"
-                if [ "$port""x" == "443x" ]; then
-                    break
-                fi
-
-                if [ "$port""x" == "80x" ]; then
-                    break
-                fi
-
-                if [ "$port""x" == "53x" ]; then
-                    break
-                fi
-
-                if [ "$port""x" == "25x" ]; then
-                    break
-                fi
-            done
-
-            pubkey=$(jq .nodes["$i"].public_key "$nodes_file"|tr -d '"')
-            status_tcp=$(jq .nodes["$i"].status_tcp "$nodes_file"|tr -d '"')
-
-            if [ "$status_tcp""x" == "true""x" ]; then
-                if [ "$port""x" != "x" ]; then
-                  if [ "$ipv4""x" != "NONEx" ]; then
-                    echo -n 'n = BootstrapNodeEntryDB_(false, num_, "'"$ipv4"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
-                    echo -n 'insert_node_into_db_real(n);' >> "$outfile"
-                    echo -n 'num_++;' >> "$outfile"
-                    echo "node #""$i:"" -> ""$port"
-                    if [ $[ $i + 1 ] != $good_nodes_count ]; then
-                            echo -n '' >> "$outfile"
-                    fi
-                    echo '' >> "$outfile"
-                  fi
-                fi
-            else
-                    :
-                    # echo "node #""$i:"
-                    # echo "node not OK"
-            fi
-        fi
-
-        i=$[ $i + 1 ]
-done
