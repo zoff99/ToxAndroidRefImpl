12847b4f2ece0948038424bdb942fc70109f2931
zoff99 <zoff99@users.noreply.github.com>
2023-05-13 12:09:00 +0200

    add debug info to node convert script, fix ipv6 lookup

--- convert_nodes_file/convert_for_trifa_android.sh
+++ convert_nodes_file/convert_for_trifa_android.sh
@@ -50,7 +50,7 @@ good_nodes_count=0
 while [ $i -lt $nodes_count ]; do
         status_udp=$(jq .nodes["$i"].status_udp "$nodes_file")
         if [ "$status_udp""x" == "true""x" ]; then
-                echo "node:$i"
+                echo "DD:000:node:$i"
                 good_nodes_count=$[ $good_nodes_count + 1 ]
 
                 ipv4=$(jq .nodes["$i"].ipv4 "$nodes_file"|tr -d '"')
@@ -58,7 +58,7 @@ while [ $i -lt $nodes_count ]; do
                 port=$(jq .nodes["$i"].port "$nodes_file"|tr -d '"')
                 pubkey=$(jq .nodes["$i"].public_key "$nodes_file"|tr -d '"')
 
-                # echo "$ipv6 $ipv4 $port $pubkey"
+                echo "DD:001:$ipv6 $ipv4 $port $pubkey"
 
                 echo -n 'n = BootstrapNodeEntryDB_(true, num_, "'"$ipv4"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
                 echo -n 'insert_node_into_db_real(n);' >> "$outfile"
@@ -67,19 +67,21 @@ while [ $i -lt $nodes_count ]; do
 
                 if [ "$ipv6""x" != "-""x" ]; then
                     if [[ $ipv6 = 2* ]] ; then
+                        echo "DD:002:ipv6"
                         echo -n 'n = BootstrapNodeEntryDB_(true, num_, "'"$ipv6"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
                         echo -n 'insert_node_into_db_real(n);' >> "$outfile"
                         echo -n 'num_++;' >> "$outfile"
                     else
                         # lookup ipv6 address
                         ipv6_lookup=$(dig AAAA +short "$ipv6" 2>/dev/null|tail -1 2>/dev/null)
+                        echo "DD:003:ipv6"
                         if [[ $ipv6_lookup = 2* ]] ; then
-                            echo "ipv6 is $ipv6_lookup"
-                            echo -n 'n = BootstrapNodeEntryDB_(true, num_, "'"$ipv6"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
+                            echo "DD:004:ipv6 is $ipv6_lookup"
+                            echo -n 'n = BootstrapNodeEntryDB_(true, num_, "'"$ipv6_lookup"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
                             echo -n 'insert_node_into_db_real(n);' >> "$outfile"
                             echo -n 'num_++;' >> "$outfile"
                         else
-                            echo "ipv6 address seems broken $ipv6_lookup"
+                            echo "DD:005:ipv6 address seems broken $ipv6_lookup"
                         fi
                     fi
                 fi
@@ -112,7 +114,7 @@ good_nodes_count=0
 while [ $i -lt $nodes_count4 ]; do
         status_tcp=$(jq .nodes["$i"].status_tcp "$nodes_file")
         if [ "$status_tcp""x" == "true""x" ]; then
-                echo "node:$i"
+                echo "DD:101:node:$i"
                 good_nodes_count4=$[ $good_nodes_count + 1 ]
 
 
@@ -122,12 +124,12 @@ while [ $i -lt $nodes_count4 ]; do
                 pubkey=$(jq .nodes["$i"].public_key "$nodes_file"|tr -d '"')
                 ports_count=$(jq '.nodes['"$i"'].tcp_ports|length' "$nodes_file"|tr -d '"')
 
-                echo "$ports len=$ports_count"
+                echo "DD:102:$ports len=$ports_count"
 
                 if [ "$ports_count""x" != "0x" ]; then
                     for tcp_port_index in $(seq 0 $(("$ports_count"-1)) ) ; do
                         port=$(jq .nodes["$i"].tcp_ports["$tcp_port_index"] "$nodes_file"|tr -d '"')
-                        echo "port=$port"
+                        echo "DD:103:port=$port"
 
 
                         if [ "$port""x" != "x" ]; then
@@ -136,7 +138,7 @@ while [ $i -lt $nodes_count4 ]; do
                             echo -n 'n = BootstrapNodeEntryDB_(false, num_, "'"$ipv4"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
                             echo -n 'insert_node_into_db_real(n);' >> "$outfile"
                             echo -n 'num_++;' >> "$outfile"
-                            echo "node #""$i:"" -> ""$port"
+                            echo "DD:104:node #""$i:"" ""$ipv4"" "" -> ""$port"
                             if [ $[ $i + 1 ] != $good_nodes_count ]; then
                                     echo -n '' >> "$outfile"
                             fi
@@ -145,20 +147,22 @@ while [ $i -lt $nodes_count4 ]; do
 
 
                             if [ "$ipv6""x" != "-""x" ]; then
+                                echo "DD:105:ipv6"
                                 if [[ $ipv6 = 2* ]] ; then
                                     echo -n 'n = BootstrapNodeEntryDB_(false, num_, "'"$ipv6"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
                                     echo -n 'insert_node_into_db_real(n);' >> "$outfile"
                                     echo -n 'num_++;' >> "$outfile"
+                                    echo "DD:106:ipv6"
                                 else
                                     # lookup ipv6 address
                                     ipv6_lookup=$(dig AAAA +short "$ipv6" 2>/dev/null|tail -1 2>/dev/null)
                                     if [[ $ipv6_lookup = 2* ]] ; then
-                                        echo "ipv6 is $ipv6_lookup"
-                                        echo -n 'n = BootstrapNodeEntryDB_(false, num_, "'"$ipv6"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
+                                        echo "DD:107:ipv6 is $ipv6_lookup"
+                                        echo -n 'n = BootstrapNodeEntryDB_(false, num_, "'"$ipv6_lookup"'",'"$port"',"'"$pubkey"'");' >> "$outfile"
                                         echo -n 'insert_node_into_db_real(n);' >> "$outfile"
                                         echo -n 'num_++;' >> "$outfile"
                                     else
-                                        echo "ipv6 address seems broken $ipv6_lookup"
+                                        echo "DD:108:ipv6 address seems broken $ipv6_lookup"
                                     fi
                                 fi
                                 echo '' >> "$outfile"
