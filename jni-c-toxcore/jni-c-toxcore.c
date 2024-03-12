a57e9b44a71e746104e187fd551626908e3a9c13
zoff99 <zoff99@users.noreply.github.com>
2024-03-12 14:07:08 +0100

    make it a define, because compiler (shrug)

--- jni-c-toxcore/jni-c-toxcore.c
+++ jni-c-toxcore/jni-c-toxcore.c
@@ -4383,11 +4383,15 @@ Java_com_zoffcc_applications_trifa_MainActivity_tox_1friend_1get_1connection_1ip
         return NULL;
     }
 
+/*
     const int ipv6_strlen_max = 39;
     const int port_strlen_max = 5;
     const int space_char_strlen_max = 1;
     const int max_tcp_relays_per_friend = 6;
     const int max_length = ((ipv6_strlen_max + space_char_strlen_max + port_strlen_max + 2) * max_tcp_relays_per_friend) + 10;
+*/
+// compiler hates humanity, so its a define now. silly.
+#define IP_STR_MAX_STR_LEN (((39 + 1 + 5 + 2) * 6) + 10)
     char ip_str[max_length + 1];
     CLEAR(ip_str);
     tox_friend_get_connection_ip(tox_global, (uint32_t)friend_number, (uint8_t *)ip_str);
