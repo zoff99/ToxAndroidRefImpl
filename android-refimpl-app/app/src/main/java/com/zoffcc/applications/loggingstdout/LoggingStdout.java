a7c9a0c70a7eb320e1c9cc89afdcf448d4867a96
zoff99 <zoff99@users.noreply.github.com>
2024-06-09 09:33:26 +0200

    clean

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/loggingstdout/LoggingStdout.java
+++
@@ -1,6 +0,0 @@
-package com.zoffcc.applications.loggingstdout;
-
-public class LoggingStdout
-{
-    public static native int start_logging();
-}
--- android-refimpl-app/app/src/main/java/org/secuso/privacyfriendlynetmonitor/Assistant/TLType.java
+++
@@ -1,9 +0,0 @@
-package org.secuso.privacyfriendlynetmonitor.Assistant;
-
-/**
- * Enum indication transport layer protocol
- */
-public enum TLType
-{
-    tcp, tcp6, udp, udp6
-}
