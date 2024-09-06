5573baf98de8a199e78dfde0b886f7363eda02a1
zoff99 <zoff99@users.noreply.github.com>
2024-09-06 15:12:29 +0200

    Antidote on iphones no longer supports push sadly. so we remove this part

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/HelperFriend.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/HelperFriend.java
@@ -1425,20 +1425,6 @@ public class HelperFriend
                                 {
                                     friend_do_actual_weburl_call(friend_pubkey, pushurl_for_friend,
                                                                  message_timestamp_circa, true);
-                                    // HINT: trigger push again after PUSH_URL_TRIGGER_AGAIN_SECONDS seconds to
-                                    //       make sure iphones actually get online and receive the message
-                                    boolean res = false;
-                                    for (int j = 0; j < PUSH_URL_TRIGGER_AGAIN_MAX_COUNT; j++)
-                                    {
-                                        Thread.sleep(PUSH_URL_TRIGGER_AGAIN_SECONDS * 1000);
-                                        res = friend_do_actual_weburl_call(friend_pubkey, pushurl_for_friend,
-                                                                           message_timestamp_circa, false);
-                                        if (res)
-                                        {
-                                            Log.i(TAG, "friend_call_push_url:BREAK");
-                                            break;
-                                        }
-                                    }
                                 }
                                 catch (Exception e)
                                 {
