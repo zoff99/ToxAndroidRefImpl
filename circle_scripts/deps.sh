1e6a59e56ce843d6c87492a25950770ca75bb369
zoff99 <zoff99@users.noreply.github.com>
2024-11-15 20:36:44 +0100

    DEBUG

--- circle_scripts/deps.sh
+++ circle_scripts/deps.sh
@@ -302,7 +302,10 @@ if [ "$full""x" == "1x" ]; then
     ls -al $_toolchain_/arm-linux-androideabi/bin/
     ln -sf $(which ar) $_toolchain_/arm-linux-androideabi/bin/arm-linux-androideabi-ar
     ln -sf $(which ranlib) $_toolchain_/arm-linux-androideabi/bin/arm-linux-androideabi-ranlib
-    ln -sf $(which strip) $_toolchain_/arm-linux-androideabi/bin/arm-linux-androideabi-strip
+    ln -sf $(which llvm-strip) $_toolchain_/arm-linux-androideabi/bin/arm-linux-androideabi-strip
+
+    type llvm-strip
+    exit 1
 
     type -a ranlib
     ls -al /usr/bin/ranlib /bin/ranlib
@@ -878,7 +881,7 @@ if [ "$full""x" == "1x" ]; then
 
     ln -sf $(which ar) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/aarch64-linux-android-ar
     ln -sf $(which ranlib) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/aarch64-linux-androideabi-ranlib
-    ln -sf $(which strip) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/aarch64-linux-androideabi-strip
+    ln -sf $(which llvm-strip) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/aarch64-linux-androideabi-strip
 
     if [ "$build_yasm""x" == "1x" ]; then
     # --- YASM ---
@@ -1456,7 +1459,7 @@ if [ "$full""x" == "1x" ]; then
 
     ln -sf $(which ar) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/i686-linux-android-ar
     ln -sf $(which ranlib) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/i686-linux-androideabi-ranlib
-    ln -sf $(which strip) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/i686-linux-androideabi-strip
+    ln -sf $(which llvm-strip) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/i686-linux-androideabi-strip
 
     if [ "$build_yasm""x" == "1x" ]; then
         # --- YASM ---
@@ -1892,7 +1895,7 @@ if [ "$full""x" == "1x" ]; then
 
     ln -sf $(which ar) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/x86_64-linux-android-ar
     ln -sf $(which ranlib) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/x86_64-linux-androideabi-ranlib
-    ln -sf $(which strip) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/x86_64-linux-androideabi-strip
+    ln -sf $(which llvm-strip) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/x86_64-linux-androideabi-strip
 
     if [ "$build_yasm""x" == "1x" ]; then
         # --- YASM ---
