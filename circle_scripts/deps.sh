89bec1312a38de9a33bbd360d503bc614cc3c5d1
zoff99 <zoff99@users.noreply.github.com>
2024-11-15 19:59:54 +0100

    ok now fix ranlib

--- circle_scripts/deps.sh
+++ circle_scripts/deps.sh
@@ -301,7 +301,7 @@ if [ "$full""x" == "1x" ]; then
     ls -al $(which ar)
     ls -al $_toolchain_/arm-linux-androideabi/bin/
     ln -sf $(which ar) $_toolchain_/arm-linux-androideabi/bin/arm-linux-androideabi-ar
-
+    ln -sf $(wich ranlib) $_toolchain_/arm-linux-androideabi/bin/arm-linux-androideabi-ranlib
 
     if [ "$build_yasm""x" == "1x" ]; then
     # --- YASM ---
@@ -872,6 +872,7 @@ if [ "$full""x" == "1x" ]; then
         --install-dir "$_toolchain_"/arm64 --api 21 --force
 
     ln -sf $(which ar) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/aarch64-linux-android-ar
+    ln -sf $(wich ranlib) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/aarch64-linux-androideabi-ranlib
 
     if [ "$build_yasm""x" == "1x" ]; then
     # --- YASM ---
@@ -1448,6 +1449,7 @@ if [ "$full""x" == "1x" ]; then
         --install-dir "$_toolchain_"/x86 --api 21 --force
 
     ln -sf $(which ar) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/i686-linux-android-ar
+    ln -sf $(wich ranlib) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/i686-linux-androideabi-ranlib
 
     if [ "$build_yasm""x" == "1x" ]; then
         # --- YASM ---
@@ -1882,6 +1884,7 @@ if [ "$full""x" == "1x" ]; then
         --install-dir "$_toolchain_"/x86_64 --api 21 --force
 
     ln -sf $(which ar) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/x86_64-linux-android-ar
+    ln -sf $(wich ranlib) $_toolchain_/$AND_TOOLCHAIN_ARCH/bin/x86_64-linux-androideabi-ranlib
 
     if [ "$build_yasm""x" == "1x" ]; then
         # --- YASM ---
