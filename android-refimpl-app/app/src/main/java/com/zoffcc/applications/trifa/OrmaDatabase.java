052f3baa8db8264b750f906e65e01bfeec817dc2
zoff99 <zoff99@users.noreply.github.com>
2025-01-22 18:45:24 +0100

    remove unused import

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/OrmaDatabase.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/OrmaDatabase.java
@@ -2,7 +2,6 @@ package com.zoffcc.applications.trifa;
 
 import android.database.Cursor;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
