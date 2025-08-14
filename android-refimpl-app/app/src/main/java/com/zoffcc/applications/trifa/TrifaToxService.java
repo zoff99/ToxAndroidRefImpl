e6f658ce84c86bffaf0fb5d20e347a2d3c19543a
zoff99 <zoff99@users.noreply.github.com>
2025-08-14 21:32:06 +0200

    trigger push before FT

--- android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/TrifaToxService.java
+++ android-refimpl-app/app/src/main/java/com/zoffcc/applications/trifa/TrifaToxService.java
@@ -1691,6 +1691,8 @@ public class TrifaToxService extends Service
                     {
                         Message m_resend_ft = (Message) ii.next();
 
+                        friend_call_push_url(m_resend_ft.tox_friendpubkey, System.currentTimeMillis());
+                        Log.i(TAG, "start_ft:sent ping to push url");
                         if (is_friend_online_real(tox_friend_by_public_key__wrapper(m_resend_ft.tox_friendpubkey)) != 0)
                         {
                             start_outgoing_ft(m_resend_ft);
