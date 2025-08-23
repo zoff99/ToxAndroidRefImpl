32a059708fe8ef15d30ac5f2cf326f701c41ff3d
zoff99 <6833516+zoff99@users.noreply.github.com>
2025-08-23 16:59:13 +0000

    update ffmpeg to n8.0

--- circle_scripts/deps.sh
+++ circle_scripts/deps.sh
@@ -17,7 +17,7 @@ build_yasm="0"
 ## ----------------------
 FORTIFY_FLAGS="" # "-D_FORTIFY_SOURCE=2"
 JNI_CUSTOM_FLAGS="-Wl,-z,max-page-size=16384" # align for 16kB
-_FFMPEG_VERSION_="n7.1"
+_FFMPEG_VERSION_="n8.0"
 _OPUS_VERSION_="v1.5.2"
 _VPX_VERSION_="v1.15.2"
 _LIBSODIUM_VERSION_="1.0.20-RELEASE"
