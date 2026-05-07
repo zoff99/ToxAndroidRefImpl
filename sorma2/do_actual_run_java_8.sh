cdc560133dc465f1250e56d22d4c037169ee6446
zoff99 <6833516+zoff99@users.noreply.github.com>
2026-05-07 15:57:25 +0000

    update sorma2

---
+++ sorma2/do_actual_run_java_8.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+cd gen/
+javac \
+-cp "sqlite-jdbc-3.53.1.0.jar" \
+com/zoffcc/applications/sorm/*.java && \
+jar cf sorma_generated.jar com/zoffcc/applications/sorm/*.class && \
+cp sorma_generated.jar ../test/ && \
+cd ../ || exit 1
+
+# use generated custom jar
+cd test/
+javac \
+-classpath ".:sqlite-jdbc-3.53.1.0.jar:sorma_generated.jar" \
+org/example/TestSorma.java || exit 1
+
+rm -f main.db
+
+java \
+-classpath ".:sqlite-jdbc-3.53.1.0.jar:sorma_generated.jar" \
+org/example/TestSorma || exit 1
--- sorma2/do_compile.sh
+++ sorma2/do_compile.sh
@@ -1,13 +1,1 @@
-#! /bin/bash
-
-javac \
--cp "sqlite-jdbc-3.53.0.0.jar" \
-com/zoffcc/applications/sorm/Column.java \
-com/zoffcc/applications/sorm/Index.java \
-com/zoffcc/applications/sorm/Log.java \
-com/zoffcc/applications/sorm/Nullable.java \
-com/zoffcc/applications/sorm/OnConflict.java \
-com/zoffcc/applications/sorm/PrimaryKey.java \
-com/zoffcc/applications/sorm/Table.java \
-com/zoffcc/applications/sorm/Generator.java || exit 1
-
+do_compile_part1_java_17_needed.sh
