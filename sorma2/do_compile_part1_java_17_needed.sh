b290720404f92cd0bdbfd3b6856650fe4cf27e8f
zoff99 <6833516+zoff99@users.noreply.github.com>
2026-05-07 15:57:25 +0000

    update sorma2

---
+++ sorma2/do_compile_part1_java_17_needed.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+javac \
+-cp "sqlite-jdbc-3.53.1.0.jar" \
+com/zoffcc/applications/sorm/Column.java \
+com/zoffcc/applications/sorm/Index.java \
+com/zoffcc/applications/sorm/Log.java \
+com/zoffcc/applications/sorm/Nullable.java \
+com/zoffcc/applications/sorm/OnConflict.java \
+com/zoffcc/applications/sorm/PrimaryKey.java \
+com/zoffcc/applications/sorm/Table.java \
+com/zoffcc/applications/sorm/Generator.java || exit 1
+
